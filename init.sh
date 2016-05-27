b618430765e39f942d337ec3926a767ec9dc7cfb
Ihor Khrystiuk <inanutshell86@gmail.com>
2016-05-27 20:12:45 +0300

    init

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
 sudo gunicorn -c /home/box/web/etc/django.py wsgi --daemon
