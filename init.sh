0c6fd4282b77b06eef958c8c6deea440ea04fa02
Ihor Khrystiuk <inanutshell86@gmail.com>
2016-05-27 20:00:27 +0300

    change init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-/etc/init.d/nginx restart
-
-gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
-gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
+cd /home/box/web/
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app
+sudo gunicorn -c /home/box/web/etc/django.py wsgi
